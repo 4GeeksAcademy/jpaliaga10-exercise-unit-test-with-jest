002d103c881b74a849fdc42049ff87b8422c9b79
jpaliaga10 <155824170+jpaliaga10@users.noreply.github.com>
2024-04-04 18:52:03 +0000

    project resuelto

---
+++ app.js
@@ -0,0 +1,24 @@
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * 156.5;
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * 0.87;
+    return valueInPound
+}
+
+module.exports = {fromDollarToYen, fromEuroToDollar, fromYenToPound}
